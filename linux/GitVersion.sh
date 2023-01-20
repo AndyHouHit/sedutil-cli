68726761d510d637ee8d56bbbbe416c95ef38a7b
Wei Hou <wei.hou@scaleflux.com>
2023-01-20 15:40:17 +0800

    add version v1.00-sfx

--- linux/GitVersion.sh
+++ linux/GitVersion.sh
@@ -1,9 +1,10 @@
 #!/bin/bash 
 set -x
 GIT=`which git`
+SFX_VER="1.00-sfx"
 if [ "x"${GIT} == "x" ]; then
-	echo "#define GIT_VERSION \"tarball\""
+        echo "#define GIT_VERSION \"$SFX_VER\""
 else
-	GITVER=`git describe --dirty`
-	echo "#define GIT_VERSION " \"$GITVER\"
+        #GITVER=`git describe --dirty`
+        echo "#define GIT_VERSION " \"$SFX_VER\"
 fi
